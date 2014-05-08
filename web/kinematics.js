91113f3472236e826d7ba7efdeff6a2b03d0439d
Austin Chen <akrolsmir@gmail.com>
2014-05-08 05:00:48 -0700

    Use numeric.js instead of three.js for math

--- web/kinematics.js
+++ web/kinematics.js
@@ -1,5 +1,3 @@
-var $N = numeric;
-
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
@@ -18,7 +16,7 @@ var render = function () {
     arm[i].update(arm[i - 1]);
   };
 
-  var dist = $N.sub(goal, arm[arm.length - 1].end.toArray());
+  var dist = $N.sub(goal, arm[arm.length - 1].end);
   if($N.norm2(dist) > 0.1) {
     // update rots for next render loop
     var rotMats = [$N.identity(3)];
@@ -27,16 +25,16 @@ var render = function () {
       rotMats.push($N.dot(arm[i].rotationMatrix(), rotMats[i - 1]));
       jcbMats.push($N.dot(rotMats[i], arm[i].jacobianMatrix()));
     }
-    var j = concatMatrices(jcbMats);
+    var j = $N.concat(jcbMats);
     var jPlus = $N.dot($N.transpose(j), $N.inv($N.dot(j, $N.transpose(j))));
         
     var dr = $N.dot(jPlus, $N.mul(dist, 0.1));
 
     for(var i = 0; i < arm.length; i++) {
-      arm[i].rot.add(new THREE.Vector3(dr[3 * i], dr[3 * i + 1], dr[3 * i + 2]));
+      $N.addeq(arm[i].rot, dr.slice(3 * i, 3 * (i + 1)));
     }
     alerted = false;
-    }
+  }
   else if(!alerted) {
     alert("DONE!");
     alerted = true;
@@ -50,9 +48,9 @@ var alerted = false;
 
 function Joint(length, rot) {
   this.length = length;
-  this.pos = new THREE.Vector3(0, 0, 0);
-  this.end = new THREE.Vector3(0, 0, 0);
-  this.rot = rot ? rot : new THREE.Vector3(0, 0, 0);
+  this.pos = [0, 0, 0];
+  this.end = [0, 0, 0];
+  this.rot = rot ? rot : [0, 0, 0];
 
   // Initializes this joint in the scene
   var material = new THREE.LineBasicMaterial({
@@ -60,8 +58,8 @@ function Joint(length, rot) {
   });
 
   var geometry = new THREE.Geometry();
-  geometry.vertices.push( this.pos );
-  geometry.vertices.push( this.end );
+  geometry.vertices.push(new THREE.Vector3().fromArray(this.pos));
+  geometry.vertices.push(new THREE.Vector3().fromArray(this.end));
 
   var line = new THREE.Line( geometry, material );
   scene.add( line );
@@ -72,32 +70,31 @@ function Joint(length, rot) {
 Joint.prototype.update = function(prev) {
   // Recalculate the pos and end based on prev and this.rot
   this.pos = prev ? prev.end : this.pos;
-  var x = prev ? prev.end.clone().sub(prev.pos).setLength(this.length) : new THREE.Vector3(this.length, 0, 0);
-  var theta = this.rot.length();
+  var x = prev ? $N.mul($N.normal($N.sub(prev.end, prev.pos)), this.length) : [this.length, 0, 0];
+  var theta = $N.norm2(this.rot);
   if (theta == 0) {
     this.end = x;
   } else {
-    var norm = this.rot.clone().normalize();
-    this.end = norm.clone().multiplyScalar(norm.dot(x))
-      .add(norm.clone().cross(x).multiplyScalar(Math.sin(theta)))
-      .sub(norm.clone().cross(norm.clone().cross(x)).multiplyScalar(Math.cos(theta)));
+    var norm = $N.normal(this.rot);
+    this.end = $N.add($N.mul(norm, $N.dot(norm, x)),
+      $N.mul($N.cross(norm, x), Math.sin(theta)),
+      $N.mul($N.cross(norm, $N.cross(norm, x)), -Math.cos(theta)));
   }
-  this.end.add(this.pos);
+  $N.addeq(this.end, this.pos);
 
   // Update the geometry
-  this.geometry.vertices[0] = this.pos;
-  this.geometry.vertices[1] = this.end;
+  this.geometry.vertices[0] = new THREE.Vector3().fromArray(this.pos);
+  this.geometry.vertices[1] = new THREE.Vector3().fromArray(this.end);
   this.geometry.verticesNeedUpdate = true;
 }
 
 Joint.prototype.jacobianMatrix = function() {
-  return crossMatrix([this.pos.x-this.end.x, this.pos.y-this.end.y, this.pos.z-this.end.z]);
+  return crossMatrix($N.sub(this.pos, this.end));
 }
 
 Joint.prototype.rotationMatrix = function() {
-  var theta = this.rot.length();
-  var norm = this.rot.clone().normalize().toArray();
-  var rx = crossMatrix(norm);
+  var theta = $N.norm2(this.rot);
+  var rx = crossMatrix($N.normal(this.rot));
   return $N.add($N.mul(rx, Math.sin(theta)), 
     $N.identity(3), 
     $N.mul($N.dot(rx, rx), 1 - Math.cos(theta)));
@@ -109,13 +106,9 @@ function crossMatrix(v) {
           [-v[2], v[1], 0]];
 }
 
-function concatMatrices(matrices) {
-  return $N.transpose(Array.prototype.concat.apply([], matrices.map($N.transpose)));
-}
-
-var joint1 = new Joint(1, new THREE.Vector3(0, 0, 0));
-var joint2 = new Joint(3, new THREE.Vector3(0, 0, 1.57));
-var joint3 = new Joint(2, new THREE.Vector3(3, 0, 0));
-var joint4 = new Joint(1, new THREE.Vector3(0.5, 0.2, 1.0));
+var joint1 = new Joint(1, [0, 0, 0]);
+var joint2 = new Joint(3, [0, 0, 1.57]);
+var joint3 = new Joint(2, [3, 0, 0]);
+var joint4 = new Joint(1, [0.5, 0.2, 1.0]);
 var arm = [joint1, joint2, joint3, joint4];
 render();
