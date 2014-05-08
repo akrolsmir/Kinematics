d27677ccd07cde81d9fe805de425d9bdbef85bde
Austin Chen <akrolsmir@gmail.com>
2014-05-07 21:57:10 -0700

    Finish inverse kinematics for a ball-joint chain

--- web/kinematics.js
+++ web/kinematics.js
@@ -1,4 +1,4 @@
-//var $N = numeric;
+var $N = numeric;
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -7,31 +7,52 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.CubeGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
-
 camera.position.z = 5;
 
 var render = function () {
   requestAnimationFrame(render);
 
-  joint.update();
-  joint2.update(joint);
-  // joint.rot.add(new THREE.Vector3(0.01, 0.01, 0.01));
-  joint.rot = new THREE.Vector3(0, 0, 0);
-  joint2.rot = new THREE.Vector3(0, 0, 1.57);
-  // console.log(joint.end);
+  // calculate endpoints
+  arm[0].update();
+  for (var i = 1; i < arm.length; i++) {
+    arm[i].update(arm[i - 1]);
+  };
+
+  var dist = $N.sub(goal, arm[arm.length - 1].end.toArray());
+  if($N.norm2(dist) > 0.1) {
+    // update rots for next render loop
+    var rotMats = [$N.identity(3)];
+    var jcbMats = [arm[0].jacobianMatrix()];
+    for(var i = 1; i < arm.length; i++) {
+      rotMats.push($N.dot(arm[i].rotationMatrix(), rotMats[i - 1]));
+      jcbMats.push($N.dot(rotMats[i], arm[i].jacobianMatrix()));
+    }
+    var j = concatMatrices(jcbMats);
+    var jPlus = $N.dot($N.transpose(j), $N.inv($N.dot(j, $N.transpose(j))));
+        
+    var dr = $N.dot(jPlus, $N.mul(dist, 0.1));
+
+    for(var i = 0; i < arm.length; i++) {
+      arm[i].rot.add(new THREE.Vector3(dr[3 * i], dr[3 * i + 1], dr[3 * i + 2]));
+    }
+    alerted = false;
+    }
+  else if(!alerted) {
+    alert("DONE!");
+    alerted = true;
+  }
 
   renderer.render(scene, camera);
 };
 
-function Joint(length) {
+var goal = [2, 1, 1];
+var alerted = false;
+
+function Joint(length, rot) {
   this.length = length;
   this.pos = new THREE.Vector3(0, 0, 0);
   this.end = new THREE.Vector3(0, 0, 0);
-  this.rot = new THREE.Vector3(0, 0, 0);
+  this.rot = rot ? rot : new THREE.Vector3(0, 0, 0);
 
   // Initializes this joint in the scene
   var material = new THREE.LineBasicMaterial({
@@ -58,8 +79,8 @@ Joint.prototype.update = function(prev) {
   } else {
     var norm = this.rot.clone().normalize();
     this.end = norm.clone().multiplyScalar(norm.dot(x))
-                .add(norm.clone().cross(x).multiplyScalar(Math.sin(theta)))
-                .sub(norm.clone().cross(norm.clone().cross(x)).multiplyScalar(Math.cos(theta)));
+      .add(norm.clone().cross(x).multiplyScalar(Math.sin(theta)))
+      .sub(norm.clone().cross(norm.clone().cross(x)).multiplyScalar(Math.cos(theta)));
   }
   this.end.add(this.pos);
 
@@ -69,6 +90,32 @@ Joint.prototype.update = function(prev) {
   this.geometry.verticesNeedUpdate = true;
 }
 
-var joint = new Joint(1);
-var joint2 = new Joint(3);
+Joint.prototype.jacobianMatrix = function() {
+  return crossMatrix([this.pos.x-this.end.x, this.pos.y-this.end.y, this.pos.z-this.end.z]);
+}
+
+Joint.prototype.rotationMatrix = function() {
+  var theta = this.rot.length();
+  var norm = this.rot.clone().normalize().toArray();
+  var rx = crossMatrix(norm);
+  return $N.add($N.mul(rx, Math.sin(theta)), 
+    $N.identity(3), 
+    $N.mul($N.dot(rx, rx), 1 - Math.cos(theta)));
+}
+
+function crossMatrix(v) {
+  return [[0, -v[2], v[1]],
+          [v[2], 0, -v[0]],
+          [-v[2], v[1], 0]];
+}
+
+function concatMatrices(matrices) {
+  return $N.transpose(Array.prototype.concat.apply([], matrices.map($N.transpose)));
+}
+
+var joint1 = new Joint(1, new THREE.Vector3(0, 0, 0));
+var joint2 = new Joint(3, new THREE.Vector3(0, 0, 1.57));
+var joint3 = new Joint(2, new THREE.Vector3(3, 0, 0));
+var joint4 = new Joint(1, new THREE.Vector3(0.5, 0.2, 1.0));
+var arm = [joint1, joint2, joint3, joint4];
 render();
